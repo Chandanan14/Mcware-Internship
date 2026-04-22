dbd13026b6afdb57e6c2086a0356acc29514fe8c
Chandana N <chandananofficial@gmail.com>
2026-04-22 12:20:45 +0530

    VotaryTech

---
+++ internship-project/script.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+ function setActiveNav() {
+    let currentPage = window.location.pathname.split("/").pop();
+
+    if (currentPage === "") {
+      currentPage = "index.html";
+    }
+
+    // 🔹 normal nav links
+    document.querySelectorAll(".nav-link").forEach(link => {
+      const href = link.getAttribute("href");
+
+      if (href === currentPage) {
+        link.classList.add("active");
+      }
+    });
+
+    // 🔹 dropdown items
+    document.querySelectorAll(".dropdown-item").forEach(item => {
+      const href = item.getAttribute("href");
+
+      if (href === currentPage) {
+        item.classList.add("active");
+
+        // 🔥 activate parent nav
+        const parentDropdown = item.closest(".dropdown");
+        if (parentDropdown) {
+          const parentLink = parentDropdown.querySelector(".nav-link");
+          if (parentLink) {
+            parentLink.classList.add("active");
+          }
+        }
+      }
+    });
+  }
+
+
+  function loadHTML(id, file, callback) {
+    fetch(file)
+      .then(res => res.text())
+      .then(data => {
+        const el = document.getElementById(id);
+        if (el) el.innerHTML = data;
+
+        if (callback) callback();
+      })
+      .catch(err => console.error("Error loading:", file, err));
+  }
+
+  // Load components
+  loadHTML("navbar", "navbar.html", setActiveNav);
+  loadHTML("footer", "footer.html");
+
+ //panels
+  const panels = document.querySelectorAll(".panel");
+  const layers = document.querySelectorAll(".bg-layer");
+
+  panels.forEach(panel => {
+    panel.addEventListener("mouseenter", () => {
+      const index = panel.dataset.bg;
+
+      layers.forEach(layer => layer.classList.remove("active"));
+
+      const target = document.querySelector(".bg" + index);
+      if (target) target.classList.add("active");
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+  
+
+});
